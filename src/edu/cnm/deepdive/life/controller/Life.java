e477f4f2864c283e706c7ac7cc77830a00013282
Ethan Lyons <ethan.lyons84@gmail.com>
2019-02-20 16:10:43 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/life/controller/Life.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.life.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class Life {
+
+  public Button start;
+  @FXML
+  private ScrollPane viewScroller;
+}
