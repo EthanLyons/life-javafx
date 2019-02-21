2f262e8c1e403e6f63cd0f05bb921b4cfcc6be64
Ethan Lyons <ethan.lyons84@gmail.com>
2019-02-21 12:01:49 -0700

    SliderFX

--- src/edu/cnm/deepdive/life/Main.java
+++ src/edu/cnm/deepdive/life/Main.java
@@ -6,33 +6,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javax.print.DocFlavor.READER;
 
 public class Main extends Application {
 
-private static final String RESOURCE_DIR = "res/";
-private static final String BUNDLE_NAME = RESOURCE_DIR + "ui-strings";
-private static final String LAYOUT_NAME = RESOURCE_DIR + "life.fxml";
-private static final String WINDOW_TITLE_KEY = "title";
+  private static final String RESOURCE_DIR = "res/";
+  private static final String BUNDLE_NAME = RESOURCE_DIR + "ui-strings";
+  private static final String LAYOUT_NAME = RESOURCE_DIR + "life.fxml";
+  private static final String WINDOW_TITLE_KEY = "title";
 
   public static void main(String[] args) {
     launch(args);
-
   }
 
   @Override
   public void start(Stage primaryStage) throws Exception {
     ClassLoader classLoader = getClass().getClassLoader();
-    ResourceBundle bundle = ResourceBundle.getBundle("res/ui-strings");
-    FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource("res/life.fxml"), bundle);
+    ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME);
+    FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource(LAYOUT_NAME), bundle);
     Parent parent = fxmlLoader.load();
     Scene scene = new Scene(parent);
     primaryStage.setScene(scene);
     primaryStage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
     primaryStage.setResizable(false);
     primaryStage.show();
-
-
-
   }
+
 }
--- src/edu/cnm/deepdive/life/view/WorldView.java
+++ src/edu/cnm/deepdive/life/view/WorldView.java
@@ -9,13 +9,13 @@ public class WorldView extends Canvas {
 
   public void draw(Cell[][] terrain) {
     GraphicsContext context = getGraphicsContext2D();
-        double cellSize = Math.min(
-            (double) getWidth() / terrain[0].length, (double) getHeight() / terrain.length);
+    double cellSize = Math.min(
+        (double) getWidth() / terrain[0].length, (double) getHeight() / terrain.length);
     context.clearRect(0, 0, getWidth(), getHeight());
     context.setFill(Color.GREEN);
-    for (int row = 0; row < terrain.length; row++)  {
-      for (int col = 0; col < terrain[row]. length; col++)  {
-        if (terrain[row][col] == Cell.ALIVE)  {
+    for (int row = 0; row < terrain.length; row++) {
+      for (int col = 0; col < terrain[row].length; col++) {
+        if (terrain[row][col] == Cell.ALIVE) {
           context.fillOval(col * cellSize, row * cellSize, cellSize, cellSize);
         }
       }
